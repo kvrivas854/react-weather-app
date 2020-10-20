e6e20a4d3d29e8054fad62ea6e70e18cd0f35941
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-20 14:57:39 -0400

    pushing input field and button

--- src/components/ErrorNotice/ErrorNotice.js
+++ src/components/ErrorNotice/ErrorNotice.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './ErrorNotice.module.css';
-import Button from './elements/Button/Button';
+import Button from '../../elements/Button/Button';
 
 const errorNotice = (props) => {
     return (
