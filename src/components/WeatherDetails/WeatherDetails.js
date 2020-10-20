05fa2015953c663610724e92d0a8d94260838329
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-20 13:45:08 -0400

    importing weather details folder and folder structure

--- src/components/WeatherDetails/WeatherDetails.js
+++ src/components/WeatherDetails/WeatherDetails.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import classes from './WeatherDetails.module.css';
+import Icon from '../../elements/Icon/Icon';
+import Temperature from './Temperature/Temperature';
+import Description from './Description/Description';
+import Date from './Date/Date';
 
-function WeatherDetails () {
+const WeatherDetails = (props) => {
     return (
-        <div></div>
+        <div className={classes.WeatherDetailsWrapper}>
+            <div className={classes.WeatherIconWrapper}>
+                <Icon type={props.data.description} />
+            </div>
+            <div className={classes.WeatherDataWrapper}>
+                <Temperature degrees={props.data.temperature} />
+                <Description type={props.data.description} />
+                <Date />
+
+            </div>
+        </div>
     );
 }
 
