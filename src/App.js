10a2137432b995b63fd7f58f927804bf88169aef
Kelly Rivas <kvrivas854@mail.cfcc.com>
2020-10-20 13:07:44 -0400

    importing folder sstructure, components, assets, elements, and container for app.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,101 @@
 import React, { Component } from 'react';
 import { MoonLoader } from 'react-spinners';
 import classes from './App.module.css';
-import Card from '../../elements/Card/Card';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
-import SearchBar from '../../components/SearchBar/SearchBar';
-import WeatherDetails from '../../components/WeatherDetails/WeatherDetails';
-import Preview from '../../components/Preview/Preview';
-import ErrorNotices from '../../components/ErrorNotice/ErrorNotice';
+import assetMapping from "./assets/assetMapping.json"
+import Card from './elements/Card/Card';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import SearchBar from './components/SearchBar/SearchBar';
+import WeatherDetails from './components/WeatherDetails/WeatherDetails';
+import Preview from './components/Preview/Preview';
+import ErrorNotice from './components/ErrorNotice/ErrorNotice';
 
 class App extends Component () {
+  state = {
+    searchBarInput: '',
+    weatherDetails: {
+      temperature: '',
+      description: '',
+    },
+    loading: false,
+    error: false
+  }
+
+  searchBarHandler = (e) => {
+    this.setState({
+      searchBarInput: e.target.value
+    })
+  }
+
+  tryAgainHandler = () => {
+    this.setState({
+      searchBarInput: '',
+      weatherDetails: {},
+      error:false
+    })
+  }
+
+  setWeather = () => {
+    const city = this.state.searchBarInput;
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const API_URL = 'https://api.openweathermap.org/data/2.5/forecast';
+    const URL = API_URL + `?q=${city}&appid=${API_KEY}&units=metric`;
+
+    this.setState({
+      weatherDetails: {},
+      loading: true,
+      error:false
+    }, () => {
+
+      fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        if(data.cod === 200) {
+          this.setState({
+            weatherDetails: {
+              temperature: data.main.temp,
+              description: data.weather[0].main
+          },
+          loading:false
+        });
+      } else {
+        throw data.cod
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      this.setState({
+        loading: false,
+        error:true
+      });
+    });
+  });
+}
   render(){
+    let cardContent = <Preview />
+    if (this.state.loading) {
+      cardContent = <MoonLoader />;
+    } else if (this.state.error) {
+      cardContent = <WeatherDetails data={this.state.weatherDetails} />;
+    }
     return (
-      <div className={classes.AppWraper}>
-        <Header />
+      <div className={classes.AppWrapper}>
+        <Header 
+        color={assetMapping.colors[
+          (this.state.error) ? "error" : this.state.weatherDetails.description
+        ]}
+        onClickHandler={this.tryAgainHandler}/>
         <main className={classes.AppMain}>
-          <SearchBar />
+          <SearchBar
+          value={this.state.searchBarInput}
+          onChangeHandler={this.searchBarHandler}
+          onClickHandler={this.setWeather}
+          error={this.state.error} />
           <Card>
-            <WeatherDetails/>
-            {/* <MoonLoader /> */}
-            {/* <Preview /> */}
-            {/* <ErrorNotice /> */}
+            {cardContent}
           </Card>
         </main>
-        <Footer />
+        <Footer onClickHandler={this.tryAgainHandler} />
       </div>
     );
   }
---
+++ src/components/ErrorNotice/ErrorNotice.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ErrorNotice () {
+    return (
+        <div></div>
+    );
+}
+
+export default ErrorNotice;
